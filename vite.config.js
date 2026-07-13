5fde27091c21f8653afce62377653e51fc71bf31
kadamprashant1 <97536014+kadamprashant1@users.noreply.github.com>
2026-07-13 14:47:07 +0530

    chages in package.json

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/prashantkadam.github.io/', // Root domain deployment (prashantkadam.github.io)
+  base: '/portfolio', // Root domain deployment (prashantkadam.github.io)
   plugins: [
     react(),
     tailwindcss(),
